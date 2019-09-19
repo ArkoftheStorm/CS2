3619b46fc5c75606378a7a36c247983c222a47dc
ArkoftheStorm <26991367+ArkoftheStorm@users.noreply.github.com>
2019-09-19 14:40:50 -0700

    colored board

--- Checkers/src/checkers/Board.java
+++ Checkers/src/checkers/Board.java
@@ -13,7 +13,21 @@ public class Board {
     public static void Draw(Graphics2D g) {
         xdelta = Window.getWidth2()/NUM_COLUMNS;
         ydelta = Window.getHeight2()/NUM_ROWS;
-        g.setColor(Color.black);
+        int c=0;
+        int r=0;
+        while (r<NUM_ROWS){
+            c = 0;
+            while (c<NUM_COLUMNS){
+                if(c%8 == 0)
+                    g.setColor(Color.black);
+                else
+                    g.setColor(Color.red);
+                g.fillRect(Window.getX(c*xdelta), Window.getY(r*ydelta), xdelta, ydelta);
+                c++;
+            }
+            r++;
+        }
+        g.setColor(Color.white);
         for (int zi=1;zi<NUM_ROWS;zi++)
         {
             g.drawLine(Window.getX(0) ,Window.getY(0)+zi*Window.getHeight2()/NUM_ROWS ,
@@ -24,5 +38,6 @@ public class Board {
             g.drawLine(Window.getX(0)+zi*Window.getWidth2()/NUM_COLUMNS ,Window.getY(0) ,
             Window.getX(0)+zi*Window.getWidth2()/NUM_COLUMNS,Window.getY(Window.getHeight2())  );
         }
+
     }
 }
