e67943ba759a7119229fa44486d428363f385d09
ArkoftheStorm <26991367+ArkoftheStorm@users.noreply.github.com>
2019-09-19 13:57:18 -0700

    window foundation

--- Checkers/src/checkers/Checkers.java
+++ Checkers/src/checkers/Checkers.java
@@ -1,6 +1,139 @@
+
 package checkers;
 
-public class Checkers {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Checkers extends JFrame implements Runnable {
+    boolean animateFirstTime = true;
+    Image image;
+    Graphics2D g;
+
+
+    public static void main(String[] args) {
+        Checkers frame = new Checkers();
+        frame.setSize(Window.WINDOW_WIDTH, Window.WINDOW_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public Checkers() {
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+
+                if (e.BUTTON1 == e.getButton() ) {
+                }
+
+                if (e.BUTTON3 == e.getButton()) {
+                }
+                repaint();
+            }
+        });
+            
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseDragged(MouseEvent e) {
+
+        repaint();
+      }
+    });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseMoved(MouseEvent e) {
+
+        repaint();
+      }
+    });
+
+        addKeyListener(new KeyAdapter() {
+
+            public void keyPressed(KeyEvent e) {
+                if (e.VK_UP == e.getKeyCode()) {
+                } else if (e.VK_DOWN == e.getKeyCode()) {
+                } else if (e.VK_LEFT == e.getKeyCode()) {
+                } else if (e.VK_RIGHT == e.getKeyCode()) {
+                } else if (e.VK_ESCAPE == e.getKeyCode()) {
+                    reset();
+                }
+                repaint();
+            }
+        });
+        init();
+        start();
+    }
+    Thread relaxer;
+////////////////////////////////////////////////////////////////////////////
+    public void init() {
+        requestFocus();
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void destroy() {
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void paint(Graphics gOld) {
+        if (image == null || Window.xsize != getSize().width || Window.ysize != getSize().height) {
+            Window.xsize = getSize().width;
+            Window.ysize = getSize().height;
+            image = createImage(Window.xsize, Window.ysize);
+            g = (Graphics2D) image.getGraphics();
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+        }
+
+        gOld.drawImage(image, 0, 0, null);
+    }
+
+////////////////////////////////////////////////////////////////////////////
+// needed for     implement runnable
+    public void run() {
+        while (true) {
+            animate();
+            repaint();
+            double seconds = .1;    //time that 1 frame takes.
+            int miliseconds = (int) (1000.0 * seconds);
+            try {
+                Thread.sleep(miliseconds);
+            } catch (InterruptedException e) {
+            }
+        }
     }
     
+/////////////////////////////////////////////////////////////////////////
+    public void reset() {
+    }
+/////////////////////////////////////////////////////////////////////////
+    public void animate() {
+
+        if (animateFirstTime) {
+            animateFirstTime = false;
+            if (Window.xsize != getSize().width || Window.ysize != getSize().height) {
+                Window.xsize = getSize().width;
+                Window.ysize = getSize().height;
+            }
+
+            reset();
+
+        }
+    }
+
+////////////////////////////////////////////////////////////////////////////
+    public void start() {
+        if (relaxer == null) {
+            relaxer = new Thread(this);
+            relaxer.start();
+        }
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void stop() {
+        if (relaxer.isAlive()) {
+            relaxer.stop();
+        }
+        relaxer = null;
+    }
+
 }
+
+
+
+
--- Checkers/src/checkers/Window.java
+++ Checkers/src/checkers/Window.java
@@ -10,5 +10,11 @@ package checkers;
  * @author 373001502
  */
 public class Window {
-    
+    private static final int XBORDER = 20;
+    private static final int YTITLE = 30;
+    private static final int WINDOW_BORDER = 8;
+    static final int WINDOW_WIDTH = 2*(WINDOW_BORDER + XBORDER) + 600;
+    static final int WINDOW_HEIGHT = YTITLE + WINDOW_BORDER + 600;
+    static int xsize = -1;
+    static int ysize = -1;
 }
