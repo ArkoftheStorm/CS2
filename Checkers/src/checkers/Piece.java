55c7f723fe9a9c97f854d8c0b138a1aa7a6b5497
Luke Hawthorne-373001502 <373001502@vusdnoc.vusd.k12.ca.us>
2019-09-23 13:45:47 -0700

    piece update

--- Checkers/src/checkers/Piece.java
+++ Checkers/src/checkers/Piece.java
@@ -1,45 +1,40 @@
-
 package checkers;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 
-
 public class Piece {
-         private Color color; 
 
-     
+    private Color color;
 
-    private static Piece board[][] = new Piece[Board.getNumRows()][ Board.getNumColumns()];
+    private static Piece board[][] = new Piece[Board.getNumRows()][Board.getNumColumns()];
 
-Piece(Color _color)   
-{
-    color = _color;
-}
+    Piece(Color _color) {
+        color = _color;
+    }
 
 //public Color getColor()
 //{
 //    return (color);
 //}
-public static void draw (Graphics2D g, int column, int row, int xdelta, int ydelta)
+    public static void draw(Graphics2D g, int column, int row, int xdelta, int ydelta) {
 
-{
-    
-      for(int zrow =0; zrow< Board.getNumRows();zrow++)
-      for(int zcol =0; zcol<  Board.getNumColumns();zcol++)
-     
-         if(board[zrow][zcol] == null)
-         {
-       
-         
-                 g.setColor(Color.red);
-                g.fillOval(Window.getX(zcol*xdelta),
-                Window.getY(zrow*ydelta),
-                xdelta,
-                ydelta);
-                
+        for (int zrow = 0; zrow < Board.getNumRows(); zrow++) {
+            for (int zcol = 0; zcol < Board.getNumColumns(); zcol++) {
+                if (Board.getColor(zrow, zcol) == Color.black) {
+                    if (board[zrow][zcol] == null) {
 
+                        g.setColor(Color.red);
+                        g.fillOval(Window.getX(zcol * xdelta),
+                                Window.getY(zrow * ydelta),
+                                xdelta,
+                                ydelta);
+
+                    }
+                }
+            }
         }
+
     }
 }
