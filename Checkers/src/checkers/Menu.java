1845cabcd31fb2749a745ac17e0eb04c2de66107
Caden <55512386+CadenKat@users.noreply.github.com>
2019-09-24 14:10:30 -0700

    menu
    it work

--- Checkers/src/checkers/Menu.java
+++ Checkers/src/checkers/Menu.java
@@ -11,9 +11,7 @@ import java.awt.Graphics2D;
 public class Menu {
 
     static boolean menuShow;
-    static int but1;
-    static int but2;
-    static int but3;
+    static boolean helpShow = false;
 
     Menu() {
 
@@ -21,51 +19,65 @@ public class Menu {
 
     public static void Reset() {
         menuShow = true;
+        helpShow = false;
     }
 
     public static void ClickButton(int xpixel, int ypixel) {
 
         if (menuShow == true) {
-          if(xpixel > Window.getX(Window.getWidth2() / 3 - 115)  
-                  && xpixel < Window.getX(Window.getWidth2() / 2 + 150) + Board.getXdelta() 
-                  && ypixel > Window.getY(Window.getHeight2() / 3 + 40)
-                  && ypixel < Window.getY(Window.getHeight2() / 3 + 70) + Board.getYdelta()){
-                
-                
-              
-                System.out.println(""+xpixel+ypixel);
+            if (xpixel > Window.getX(Window.getWidth2() / 3 - 115) && xpixel < Window.getX(Window.getWidth2() / 2 + 120) + Board.getXdelta() && ypixel > Window.getY(Window.getHeight2() / 3 + 33) && ypixel < Window.getY(Window.getHeight2() / 3 + 33) + Board.getYdelta()) {
+                System.out.println("" + xpixel + ypixel);
                 menuShow = false;
-          }
+            } else if (xpixel > Window.getX(Window.getWidth2() / 3 - 115) && xpixel < Window.getX(Window.getWidth2() / 2 + 120) + Board.getXdelta() && ypixel > Window.getY(Window.getHeight2() / 3 + 130) && ypixel < Window.getY(Window.getHeight2() / 3 + 130) + Board.getYdelta()) {
+                System.out.println("Help = " + xpixel + ypixel);
+                helpShow = true;
+                menuShow = false;
+            }
         }
     }
 
     public static void draw(Graphics g) {
-        Color menuColor = new Color(91, 195, 255);
+        Color menuColor = new Color(255, 140, 138);
 
-        if (menuShow == false) {
-            return;
-        }
+        if (menuShow == true) {
 
-        g.setColor(menuColor);
-        g.fillRect(0, 0, Window.xsize, Window.ysize);
+            g.setColor(menuColor);
+            g.fillRect(0, 0, Window.xsize, Window.ysize);
 
 //"PLAY" BUTTON        
-        g.setColor(Color.red);
-        g.fillRect(Window.getWidth2() / 3 - 3, Window.getHeight2() / 2 - 3, 406, 57);
+            g.setColor(Color.red);
+            g.fillRect(Window.getWidth2() / 3 - 3, Window.getHeight2() / 2 - 3, 406, 57);
 
-        g.setColor(Color.black);
-        g.fillRect(Window.getWidth2() / 3, Window.getHeight2() / 2, 400, 50);
+            g.setColor(Color.black);
+            g.fillRect(Window.getWidth2() / 3, Window.getHeight2() / 2, 400, 50);
 
-        g.setColor(Color.red);
-        g.setFont(new Font("Segoe UI Semibold", Font.BOLD, 55));
-        g.drawString("PLAY", Window.getWidth2() / 3 + 130, Window.getHeight2() / 2 + 45);
+            g.setColor(Color.red);
+            g.setFont(new Font("Segoe UI Semibold", Font.BOLD, 55));
+            g.drawString("PLAY", Window.getWidth2() / 3 + 130, Window.getHeight2() / 2 + 45);
+
+//"HOW TO PLAY" BUTTON        
+            g.setColor(Color.red);
+            g.fillRect(Window.getWidth2() / 3 - 3, Window.getHeight2() / 2 + 100, 406, 57);
+
+            g.setColor(Color.black);
+            g.fillRect(Window.getWidth2() / 3, Window.getHeight2() / 2 + 103, 400, 50);
+
+            g.setColor(Color.red);
+            g.setFont(new Font("Segoe UI Semibold", Font.BOLD, 53));
+            g.drawString("HOW TO PLAY", Window.getWidth2() / 3 + 23, Window.getHeight2() / 2 + 147);
 
 //"CHECKERS"
-        g.setColor(Color.black);
-        g.setFont(new Font("Segoe UI Semibold", Font.BOLD, 125));
-        g.drawString("CHECKERS", Window.getWidth2() / 5, Window.getHeight2() / 4);
+            g.setColor(Color.black);
+            g.setFont(new Font("Segoe UI Semibold", Font.BOLD, 125));
+            g.drawString("CHECKERS", Window.getWidth2() / 5 - 15, Window.getHeight2() / 4);
+        }
+        if (helpShow == true) {
 
-    
+            g.setColor(menuColor);
+            g.fillRect(0, 0, Window.xsize, Window.ysize);
+
+        }
+
     }
 
 }
