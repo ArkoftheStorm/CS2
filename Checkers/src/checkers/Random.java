57bdf96f4b7ea31dac511ba815aa561c8ad5d811
ArkoftheStorm <26991367+ArkoftheStorm@users.noreply.github.com>
2019-09-24 14:39:42 -0700

    random 1

--- Checkers/src/checkers/Random.java
+++ Checkers/src/checkers/Random.java
@@ -7,11 +7,12 @@ public class Random {
     public static void Draw(Graphics2D g) {
         g.setColor(Color.yellow);
         g.fillRect(Window.getX(Window.getWidth2() + 10), Window.getY(0), Board.getXdelta(), Board.getYdelta());
+        g.setFont (new Font("times new roman",Font.PLAIN,24));
     }
     public static void Roll(Graphics2D g) {
         int roll = (int)(Math.random()*3);
         if(roll == 1){
-            
+            Player.GetCurrentPlayer().changeCount++;
         }
         else if(roll == 2){
             
@@ -19,5 +20,6 @@ public class Random {
         else if(roll == 3){
             
         }
+        Player.SwitchTurn();
     }
 }
