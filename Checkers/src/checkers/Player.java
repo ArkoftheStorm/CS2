93ecc5c37797b090c6398f9fac2027c7792a0692
ArkoftheStorm <26991367+ArkoftheStorm@users.noreply.github.com>
2019-09-25 13:54:21 -0700

    stuff

--- Checkers/src/checkers/Player.java
+++ Checkers/src/checkers/Player.java
@@ -6,17 +6,17 @@ public class Player {
 
     private static Player currentTurn;
     private static Player players[] = new Player[2];
-    private Color color;
-    int changeCount;
+    public int changeCount;
 
     public static void Reset() {
 
         if (players[0] == null) {
-            players[0] = new Player(Color.red);
-            players[1] = new Player(Color.black);
+            players[0] = new Player(0);
+            players[1] = new Player(0);
         }
         currentTurn = players[0];
-
+        getPlayer(0).changeCount = 0;
+        getPlayer(1).changeCount = 0;
     }
 
     public static Player GetCurrentPlayer() {
@@ -30,14 +30,13 @@ public class Player {
                         : players[1];
     }
 
-    Player(Color _color) {
-        color = _color;
-
+    Player(int _changeCount) {
+        changeCount = 0;
     }
 
-    public Color getColor() {
-        return (color);
-    }
+//    public Color getColor() {
+//        return (color);
+//    }
     public static Player getPlayer(int p){
         return players[p];
     }
--- Checkers/src/checkers/Random.java
+++ Checkers/src/checkers/Random.java
@@ -5,9 +5,13 @@ import java.awt.*;
 public class Random {
 
     public static void Draw(Graphics2D g) {
+        if(Menu.menuShow || Menu.helpShow)
+            return;
         g.setColor(Color.yellow);
         g.fillRect(Window.getX(Window.getWidth2() + 10), Window.getY(0), Board.getXdelta(), Board.getYdelta());
-        g.setFont (new Font("times new roman",Font.PLAIN,24));
+        g.setFont (new Font("times new roman",Font.BOLD,18));
+        g.drawString("P1 Changes: " + Player.getPlayer(0).changeCount, Window.getWidth2()/64, Window.getY(Window.getHeight2())+18);
+        g.drawString("P2 Changes: " + Player.getPlayer(1).changeCount, Window.getWidth2(), Window.getY(Window.getHeight2())+18);
     }
     public static void Roll(Graphics2D g) {
         int roll = (int)(Math.random()*3);
