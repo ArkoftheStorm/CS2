28dff9f5a3d41b360499c78e251ec08112bca229
Caden <55512386+CadenKat@users.noreply.github.com>
2019-09-30 14:39:20 -0700

    push code,
    i must.

--- Checkers/src/checkers/Menu.java
+++ Checkers/src/checkers/Menu.java
@@ -105,7 +105,7 @@ public class Menu {
 //"CHECKERS"
             g.setColor(Color.black);
             g.setFont(new Font("Segoe UI Semibold", Font.BOLD, 125));
-            g.drawString("CHECKERS", Window.getWidth2() / 5 - 15, Window.getHeight2() / 4);
+            g.drawString("cHECKERS", Window.getWidth2() / 5 - 15, Window.getHeight2() / 4);
             g.setFont(new Font("Segoe UI Semibold", Font.BOLD, 20));
             g.drawString("Made By: Caden Y, Luke H, and Joshua H ", Window.getWidth2() / 8 - 50, Window.getHeight2() + 50);
             g.drawString("(Don't Change Screen Size)", Window.getWidth2() / 8 + 475, Window.getHeight2() + 50);
@@ -116,23 +116,27 @@ public class Menu {
             g.fillRect(0, 0, Window.xsize, Window.ysize);
 
             g.setColor(Color.black);
-            g.fillRect(Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 5, 656, 10);
+            g.fillRect(Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 - 25, 656, 10);
 
             g.setColor(Color.black);
 
             g.setFont(new Font("Segoe UI Semibold", Font.BOLD, 100));
-            g.drawString("HOW TO PLAY", Window.getWidth2() / 5 - 50, Window.getHeight2() / 4);
+            g.drawString("HOW TO PLAY", Window.getWidth2() / 5 - 50, Window.getHeight2() / 4 - 30);
 
             g.setFont(new Font("Segoe UI Semibold", Font.BOLD, 25));
-            g.drawString("Step 1: Decide who is player 1 and who is player 2. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 50);
-            g.drawString("Step 2: Player 1 goes first. They are the bottom. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 100);
-            g.drawString("Player 2 goes second. They are the top. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 130);
-            g.drawString("Step 3: Click on the space where you want to move (Diagonal). ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 180);
-            g.drawString("Step 4: If there is a piece at a spot you can move to, ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 230);
-            g.drawString("and there is a empty spot diagonal to them in the way that you are ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 260);
-            g.drawString("moving, you can hop over them. This eliminates thier piece. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 290);
-            g.drawString("Step 5: Get one of your pieces to the end to make it a king. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 330);
-            g.drawString("Kings can move in all directions (Diagonal). ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 360);
+            g.drawString("Step 1: Decide who is player 1 and who is player 2. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 10);
+            g.drawString("Step 2: Player 1 goes first. They are the bottom. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 40);
+            g.drawString("Player 2 goes second. They are the top. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 70);
+            g.drawString("Step 3: Click on the space where you want to move (Diagonal). ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 100);
+            g.drawString("Step 4: If there is a piece at a spot you can move to, ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 130);
+            g.drawString("and there is a empty spot diagonal to them in the way that you are ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 160);
+            g.drawString("moving, you can hop over them. This eliminates thier piece. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 190);
+            g.drawString("Step 5: Get one of your pieces to the end to make it a king. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 220);
+            g.drawString("Kings can move in all directions (Diagonal). ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 250);
+            g.drawString("When the yellow button is pressed, you use a power. ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 280);
+            g.drawString("There are three powers. One gives you an extra turn,", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 310);
+            g.drawString("The second one lets you blow up some pieces, and the", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 340);
+            g.drawString("third one lets you change the color of a piece.", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 370);
 
             g.setFont(new Font("Segoe UI Semibold", Font.BOLD, 30));
             g.drawString("How to Win: Eliminate all of the enemy pieces! ", Window.getWidth2() / 5 - 44, Window.getHeight2() / 4 + 410);
