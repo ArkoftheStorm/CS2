8d1b823358f805016a4d157611a248c23556dd66
ArkoftheStorm <26991367+ArkoftheStorm@users.noreply.github.com>
2019-09-30 13:32:38 -0700

    c

--- Checkers/src/checkers/Board.java
+++ Checkers/src/checkers/Board.java
@@ -78,7 +78,7 @@ public class Board {
         if(Player.GetCurrentPlayer().getChangeCount() <= 0)
             return;
         int _col = (xPixel/xdelta) - 1;
-        int _row = (yPixel/xdelta);
+        int _row = (yPixel/ydelta);
         if(Pieces[_row][_col] != null && _row < NUM_ROWS && _col < NUM_COLUMNS)
             Pieces[_row][_col].switchColor();
         Player.GetCurrentPlayer().decChangeCount();
